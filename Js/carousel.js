42f81b3a87fe75958c506fd6f6d0b0e64065f1eb
friskaim7 <58653759+friskaim7@users.noreply.github.com>
2020-06-14 16:48:29 +0700

    add Js folder
    add chart.js and jquery.canvasjs.min.js,
    move carousel.js from Assets to Js folder

---
+++ Js/carousel.js
@@ -0,0 +1,128 @@
+var ctx = document.getElementById('TestChart');
+var myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132)',
+            'rgba(54, 162, 235)',
+            'rgba(255, 206, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(153, 102, 255)',
+            'rgba(255, 159, 64)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132)',
+            'rgba(54, 162, 235)',
+            'rgba(255, 206, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(153, 102, 255)',
+            'rgba(255, 159, 64)'
+        ],
+           borderWidth: 1
+      }]
+  },
+  options: {
+      title:{
+          display: true,
+          text: 'Left PieChart',
+          fontSize: 25
+      }
+  }
+});
+
+var ctx = document.getElementById('TestChart2');
+  var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [1, 13, 31, 25, 12, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+             borderWidth: 1
+        }]
+    },
+    options: {
+        title:{
+            display: true,
+            text: 'Middle PieChart',
+            fontSize: 25
+        }
+    }
+});
+
+var ctx = document.getElementById('TestChart3');
+  var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [16, 11, 23, 5, 12, 13],
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+             borderWidth: 1
+        },
+        {
+            label: '# of takes',
+            data: [1, 13, 31, 25, 12, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)'
+            ],
+             borderWidth: 1,
+        }]
+    },
+    options: {
+        title:{
+            display: true,
+            text: 'Right PieChart',
+            fontSize: 25
+        }
+    }
+});
